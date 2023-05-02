f025736dd821ed4459241e5474088860dc75b96c
ajanderson26 <117036528+ajanderson26@users.noreply.github.com>
2023-05-02 15:08:02 -0500

    update to final map

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 //initilize map and set center of map with coordinates
 var map2;
-function createmap(){
+/*function createmap(){
 
  map2 = L.map('map2', {
     center: [-152.4691482, 36.1755150],
@@ -8,12 +8,29 @@ function createmap(){
     minzoom:5.2,
     maxzoom:6.7
 });
-};
+};*/
+//function to create the Leaflet map
+function createMap() {
+    //create the map
+    map2 = L.map('map2', {
+        center: [-152.4691482, 36.1755150],
+        zoom: 2,
+        minZoom: 2,
+        maxZoom: 15,
+    scrollWheelZoom: false,
+    doubleClickZoom: true
+
+    });
+
 
     //Step 1: add the openstreet map tilelayer
     L.tileLayer('https://api.mapbox.com/styles/v1/ajanderson26/cl9yx108p002o15r70hbmu73w/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYWphbmRlcnNvbjI2IiwiYSI6ImNsOXl3dnIzdzAwNmszcW1yMmhrZjlsNHUifQ.XCys49mvEy12hmZV60I_9A', {
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>'
-    }).addTo(map2);
+    }).addTo(map);
+
+    //call getData function
+    getData();
+};
 
 //what link to useee here? 
 /*L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -33,5 +50,5 @@ function getData() {
 };
 
 
-createmap();
-document.addEventListener('DOMContentLoaded', createmap2)
+//createMap();
+document.addEventListener('DOMContentLoaded', createMap)
